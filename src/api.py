6ed8f8bf851cb6fe433f85a0bc84be6812d5978e
Thundelly <thundelly0609@gmail.com>
2021-03-05 05:22:33 -0800

    Commented on api.py

--- src/api.py
+++ src/api.py
@@ -5,13 +5,17 @@ from flask_cors import CORS
 from endpoints import Search
 from search_engine import SearchEngine
 
-
+# Create a flask api
 app = Flask(__name__)
 api = Api(app)
+# Allow Cross Origin Access. This is used for http / https compatibility
 CORS(app)
 
+# Make an instance of a search engine
 search_engine = SearchEngine()
-
+# Add an api endpoint
+# Pass the search engine instance to allow endpoint
+# functions to use the engine
 api.add_resource(Search, '/search',
                  resource_class_kwargs={'search_engine': search_engine})
 
